3dbdc48d17fefaa66662585e57f7aa0094d3b314
Fabricio Pirini <fabricio.pirini@ngn.no>
2023-05-28 18:09:17 +0200

    Add Navbar and Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      textColor: {
+        primary: {
+          DEFAULT: 'var(--color-text-primary)',
+        },
+        accent: {
+          DEFAULT: 'var(--color-text-accent)',
+        }
       },
+      backgroundColor: {
+        primary: {
+          DEFAULT: 'var(--color-bg-primary)',
+        },
+        secondary: {
+          DEFAULT: 'var(--color-bg-secondary)',
+          hover: 'var(--color-bg-secondary-hover)',
+        },
+      }
     },
   },
   plugins: [],

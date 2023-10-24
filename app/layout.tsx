f0913d4698a584af8c0048077d0dfbf60586debd
Fabricio Pirini <fabricio.pirini@ngn.no>
2023-10-24 19:38:51 +0200

    Update hero info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Analytics } from '@vercel/analytics/react'
 import { League_Spartan } from 'next/font/google'
 
+import { HeroPattern } from '@/components/HeroPattern'
+
 import '@/app/globals.css'
 
 const inter = League_Spartan({ subsets: ['latin'] })
@@ -13,8 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${process.env.VERCEL_ENV === 'production' ? '' : 'debug-screens'}`}>
+      <body
+        className={`${inter.className} overflow-x-hidden ${
+          process.env.VERCEL_ENV === 'production' ? '' : 'debug-screens'
+        }`}
+      >
         {children}
+        {/* <HeroPattern /> */}
         <Analytics />
       </body>
     </html>
--- components/Name/index.tsx
+++ components/Name/index.tsx
@@ -1,81 +1,96 @@
 'use client'
 import { motion } from 'framer-motion'
-import { GlobeIcon, Laptop2Icon, Linkedin, Mail } from 'lucide-react'
+import { Clock7Icon, GlobeIcon, Laptop2Icon, Linkedin, Mail } from 'lucide-react'
 
 import { CTAButton } from '@/components/CTAButton'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { FADE_DOWN_ANIMATION_VARIANTS, FADE_RIGHT_ANIMATION_VARIANTS } from '@/lib/constants'
 
-export const NameBanner = () => (
-  <motion.div
-    initial="hidden"
-    animate="show"
-    viewport={{ once: true }}
-    variants={{
-      hidden: {},
-      show: {
-        transition: {
-          staggerChildren: 0.15,
+export const NameBanner = () => {
+  const date1 = new Date('2017-09-01') // 1 year before I started working as a developer at Sportradar (that includes my internship)
+  const date2 = new Date()
+
+  const diffTime = Math.abs(date2.valueOf() - date1.valueOf())
+  const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365))
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
         },
-      },
-    }}
-  >
-    <motion.p className="flex flex-col gap-3 pb-5 sm:flex-row sm:items-center" variants={FADE_DOWN_ANIMATION_VARIANTS}>
-      <span>I&apos;m</span>
-      <span className="inline-block text-5xl font-medium text-accent md:text-6xl">Fabricio Pirini</span>
-    </motion.p>
-    <motion.p className="flex flex-row items-baseline gap-3" variants={FADE_DOWN_ANIMATION_VARIANTS}>
-      <Laptop2Icon size={20} className="inline-block" />
-      Frontend Engineer
-    </motion.p>
-    <motion.p className="mb-4 flex flex-row items-baseline gap-3" variants={FADE_DOWN_ANIMATION_VARIANTS}>
-      <GlobeIcon size={20} className="inline-block" />
-      Based in Norway
-    </motion.p>
-    <motion.div className="flex flex-col-reverse gap-6 pb-8 sm:flex-row">
-      <CTAButton type="secondary" external text="Resume" href="https://bit.ly/fabriciopirini-updated-CV" isAnimated />
-      <Popover>
-        <PopoverTrigger asChild>
-          <CTAButton type="primary" text="Contact me" isAnimated />
-        </PopoverTrigger>
-        <PopoverContent className="mt-4 rounded-xl border-2 border-accent bg-white drop-shadow-md" align="center">
-          <motion.div
-            initial="hidden"
-            animate="show"
-            viewport={{ once: true }}
-            variants={{
-              hidden: {},
-              show: {
-                transition: {
-                  staggerChildren: 0.15,
+      }}
+    >
+      <motion.p
+        className="flex flex-col gap-3 pb-5 sm:flex-row sm:items-center"
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+      >
+        <span>I&apos;m</span>
+        <span className="inline-block text-5xl font-medium text-accent md:text-6xl">Fabricio Pirini</span>
+      </motion.p>
+      <motion.p className="flex flex-row items-baseline gap-3" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+        <Laptop2Icon size={20} className="inline-block" />
+        Senior Frontend Engineer
+      </motion.p>
+      <motion.p className="flex flex-row items-baseline gap-3" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+        <Clock7Icon size={20} className="inline-block" />
+        {diffYears ?? 6}+ years of experience
+      </motion.p>
+      <motion.p className="mb-4 flex flex-row items-baseline gap-3" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+        <GlobeIcon size={20} className="inline-block" />
+        Based in Norway
+      </motion.p>
+      <motion.div className="flex flex-col-reverse gap-6 pb-8 sm:flex-row">
+        <CTAButton type="secondary" external text="Resume" href="https://bit.ly/fabriciopirini-updated-CV" isAnimated />
+        <Popover>
+          <PopoverTrigger asChild>
+            <CTAButton type="primary" text="Contact me" isAnimated />
+          </PopoverTrigger>
+          <PopoverContent className="mt-4 rounded-xl border-2 border-accent bg-white drop-shadow-md" align="center">
+            <motion.div
+              initial="hidden"
+              animate="show"
+              viewport={{ once: true }}
+              variants={{
+                hidden: {},
+                show: {
+                  transition: {
+                    staggerChildren: 0.15,
+                  },
                 },
-              },
-            }}
-            className="flex flex-row items-center justify-around gap-3"
-          >
-            <motion.a
-              href="mailto:fabriciopirini@gmail.com"
-              className="flex flex-col gap-2"
-              variants={FADE_RIGHT_ANIMATION_VARIANTS}
+              }}
+              className="flex flex-row items-center justify-around gap-3"
             >
-              <div className="rounded-full bg-primary p-4">
-                <Mail size={32} className="mx-auto inline-block" />
-              </div>
-              <span className="mx-auto text-lg text-primary-background">Email</span>
-            </motion.a>
-            <motion.a
-              href="https://www.linkedin.com/in/fabriciopirini/"
-              className="flex flex-col items-center justify-center gap-2"
-              variants={FADE_RIGHT_ANIMATION_VARIANTS}
-            >
-              <div className="rounded-full bg-primary p-4">
-                <Linkedin size={32} className="mx-auto inline-block" />
-              </div>
-              <span className="mx-auto text-lg text-primary-background">LinkedIn</span>
-            </motion.a>
-          </motion.div>
-        </PopoverContent>
-      </Popover>
+              <motion.a
+                href="mailto:fabriciopirini@gmail.com"
+                className="flex flex-col gap-2"
+                variants={FADE_RIGHT_ANIMATION_VARIANTS}
+              >
+                <div className="rounded-full bg-primary p-4">
+                  <Mail size={32} className="mx-auto inline-block" />
+                </div>
+                <span className="mx-auto text-lg text-primary-background">Email</span>
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/fabriciopirini/"
+                className="flex flex-col items-center justify-center gap-2"
+                variants={FADE_RIGHT_ANIMATION_VARIANTS}
+              >
+                <div className="rounded-full bg-primary p-4">
+                  <Linkedin size={32} className="mx-auto inline-block" />
+                </div>
+                <span className="mx-auto text-lg text-primary-background">LinkedIn</span>
+              </motion.a>
+            </motion.div>
+          </PopoverContent>
+        </Popover>
+      </motion.div>
     </motion.div>
-  </motion.div>
-)
+  )
+}

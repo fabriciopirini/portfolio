1e6c19996f488e57e8c1b70787dc7189870f9497
Fabricio Pirini <fabricio.pirini@ngn.no>
2024-05-05 20:21:05 +0200

    Round up years of experience

--- components/HeroLegoImage.tsx
+++ components/HeroLegoImage.tsx
@@ -12,7 +12,7 @@ export const HeroLegoImage = () => {
   const date2 = new Date()
 
   const diffTime = Math.abs(date2.valueOf() - date1.valueOf())
-  const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365))
+  const diffYears = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 365))
 
   useEffect(() => {
     const adjustFontSize = () => {

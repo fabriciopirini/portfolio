2497a43350ada0c36c1ff61671aeb1eec65573a0
Fabricio Pirini <fabricio.pirini@gmail.com>
2025-10-17 14:32:15 -0300

    Refactor PostHog integration: update initialization parameters, add API rewrites for ingestion, and enable exception capturing.

--- components/Providers/index.tsx
+++ components/Providers/index.tsx
@@ -7,12 +7,6 @@ if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not defined')
 }
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  })
-}
-
 export const CSPostHogProvider = ({ children }: { children: React.ReactNode }) => {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }

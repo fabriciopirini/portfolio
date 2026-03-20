970bccad88778fdf3ffbcdc01300fa1e8dedaa64
Fabricio Pirini <fabricio.pirini@gmail.com>
2026-03-19 22:11:20 -0300

    feat: setup reverse proxy against adblockers

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,9 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: '/ingest',
+  api_host: 'https://t.fabriciopirini.com',
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: '2025-05-24',
+  defaults: '2026-01-30',
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking
   debug: process.env.NODE_ENV === 'development',
 })
